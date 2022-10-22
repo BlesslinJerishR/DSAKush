4c053818b1bdaaaae9520a6d5cdfd35118f66ae2
Mastermind <mastermindx33@gmail.com>
2022-10-22 13:23:58 +0530

    Prime number + Python

--- Python/prime_number.py
+++ Python/prime_number.py
@@ -4,6 +4,7 @@ number = abs(int(input("Enter a number to check whether it is a prime or not : "
 # To check greater than 1
 if number > 1:
     for num in range(2, number):
+        # # To Debug print
         # print(number,num)
         if number % num == 0:
             print("Not Prime number")
