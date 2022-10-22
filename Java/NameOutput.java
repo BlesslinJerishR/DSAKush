c894e40b27989011cdfd67261b7eb7fdc6e06c0b
Mastermind <mastermindx33@gmail.com>
2022-10-22 12:27:27 +0530

    NameOutput Java

---
+++ Java/NameOutput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class NameOutput{
+    public static void main(String[] args) {
+
+        String name;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a name :- ");
+        name = sc.nextLine();
+        System.out.println("Hello "+ name);
+
+    }
+}
