102520f7c90feed1c022a23001b3a7ca61c86ea5
Mastermind <mastermindx33@gmail.com>
2022-11-07 21:04:46 +0530

    celicus to fahrenheit

---
+++ Python/celcius_2_fahrenheit.py
@@ -0,0 +1,18 @@
+class celcius_2_fahrenheit:
+
+    def Temperature():
+        choice = int(input("""Enter the Conversion you need to convert
+        1. Celcius to Fahrenheit
+        2. Fahrenheit to Celcius
+        >> """))
+        if choice == 1:
+            celcius = int(input("""Enter the temperature in degree celcius 
+        >> """))
+            fahrenheit = (celcius*1.8) + 32
+            print("The converted temperature is " , fahrenheit)
+        if choice == 2:
+           fahrenheit = int(input("""Enter the temperature in degrees fahrenheit
+           >> """))
+           celcius = (fahrenheit - 32) * (5/9)
+           print("The converted temperature is " , celcius)
+    Temperature()
