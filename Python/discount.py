92a4eb2e2638faad96cbd1901657828739aad343
Mastermind <mastermindx33@gmail.com>
2022-11-22 12:38:06 +0530

    discount.py

---
+++ Python/discount.py
@@ -0,0 +1,6 @@
+class discount:
+    def discount():
+        sales = int(input("Enter the sales price : "))        
+        discount = sales * .85
+        print(f"The discount price is {discount}")
+    discount()
